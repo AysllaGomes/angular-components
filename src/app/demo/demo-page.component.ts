fc75898d4ca4af3ef787cf8ef3a96c7f8773a392
Ayslla Gomes <ayslla.gomes@outlook.com>
2025-09-02 13:40:43 -0300

    [Added] [AC-???] Adicionando no demo-page e header

--- src/app/demo/demo-page.component.ts
+++ src/app/demo/demo-page.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { TableDemoComponent } from './sections/table/table-demo.component';
 import { StepperDemoComponent } from './sections/stepper/stepper-demo.component';
 
 @Component({
   selector: 'app-demo-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, StepperDemoComponent],
+  imports: [HeaderComponent, FooterComponent, StepperDemoComponent, TableDemoComponent],
   templateUrl: './demo-page.component.html',
   styleUrl: './demo-page.component.sass'
 })
